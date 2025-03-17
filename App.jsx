ba667f889a9d82cd991a3c0beb7ed6ff7d7f5488
tyrimbet abylaikhan <abeke.618@gmail.com>
2025-03-17 18:58:59 +0500

    done

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import Ifcoroch from "./ifcocrch";
+import Ifcoroch2 from "./ifcoroch2";
+
+export default function App() {
+    return(
+        <div>
+            <Ifcoroch/>
+            <Ifcoroch2/>
+        </div>
+    )
+w}
